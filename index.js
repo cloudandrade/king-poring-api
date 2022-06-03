da902c5d0d4038ca7fb0ce8f3bd7aa246a49d829
Jan Andrade <cloud.andrade16@gmail.com>
2022-06-02 22:50:26 -0300

    cleaning swagger document

--- index.js
+++ index.js
@@ -3,28 +3,28 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 require('./src/config/sequelizeConfig');
 
-
 const cors = require('cors');
 
 const Logger = require('./src/utils/logger');
 const logger = new Logger('Server');
 
-const swaggerJsDoc = require('swagger-jsdoc');
+//const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 
-const {swaggerOptions} = require('./src/config/swaggerConfig')
+//const {swaggerDocument} = require('./src/config/swaggerConfig')
+const swaggerDocument = require('./swagger.json');
 
-logger.debug("## Start Checking Environment Variables ##")
-logger.debug("API_PORT: " + process.env.API_PORT)
-logger.debug("API_HOST: " + process.env.API_HOST)
-logger.debug("DBNAME: " + process.env.DBNAME)
-logger.debug("DBUSER: " + process.env.DBUSER)
-logger.debug("DBPASS: " + process.env.DBPASS)
-logger.debug("DBHOST: " + process.env.DBHOST)
-logger.debug("DBPORT: " + process.env.DBPORT)
-logger.debug("JWT_SECRET: " + process.env.JWT_SECRET)
-logger.debug("LOG_SERVICE_TYPE: " + process.env.LOG_SERVICE_TYPE)
-logger.debug("## End Checking Environment Variables ##")
+logger.debug('## Start Checking Environment Variables ##');
+logger.debug('API_PORT: ' + process.env.API_PORT);
+logger.debug('API_HOST: ' + process.env.API_HOST);
+logger.debug('DBNAME: ' + process.env.DBNAME);
+logger.debug('DBUSER: ' + process.env.DBUSER);
+logger.debug('DBPASS: ' + process.env.DBPASS);
+logger.debug('DBHOST: ' + process.env.DBHOST);
+logger.debug('DBPORT: ' + process.env.DBPORT);
+logger.debug('JWT_SECRET: ' + process.env.JWT_SECRET);
+logger.debug('LOG_SERVICE_TYPE: ' + process.env.LOG_SERVICE_TYPE);
+logger.debug('## End Checking Environment Variables ##');
 
 //EXPRESS
 const app = express();
@@ -39,28 +39,29 @@ app.use(bodyParser.json());
 app.use(cors());
 
 //configuring swagger
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+//const swaggerDocs = swaggerJsDoc(swaggerDocument);
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument)
+);
 
-logger.debug("## Swagger Configuration Done! ##")
-logger.debug(`Swagger documentation available on ${process.env.API_HOST}:${process.env.API_PORT}/api-docs`)
-
-
+logger.debug('## Swagger Configuration Done! ##');
+logger.debug(
+  `Swagger documentation available on ${process.env.API_HOST}:${process.env.API_PORT}/api-docs`
+);
 
 //ROUTES
 app.use('/', require('./src/routes'));
 
-
 //SERVER
-app.listen(
-	 PORT,
-    async () => {
-        if(process.env.LOG_SERVICE_TYPE === 'LOG' || process.env.LOG_SERVICE_TYPE === 'BOTH'){
-            logger.info(`Servidor Iniciado na Porta: ${PORT}`.bgBlue)
-        }else{
-            logger.info(`Servidor Iniciado na Porta: ${PORT}`) 
-        }
-        
-    }
-	
-);
+app.listen(PORT, async () => {
+  if (
+    process.env.LOG_SERVICE_TYPE === 'LOG' ||
+    process.env.LOG_SERVICE_TYPE === 'BOTH'
+  ) {
+    logger.info(`Servidor Iniciado na Porta: ${PORT}`.bgBlue);
+  } else {
+    logger.info(`Servidor Iniciado na Porta: ${PORT}`);
+  }
+});
